6eae75ff852b41ca790b31f1785fbf9f0dff815d
Luziffer090422 <luca.knobloch@web.de>
2019-12-03 21:51:06 +0300

    update gameplay

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ Uses PyopenGL to create a simple soccer game
 
 Features <br>
     [ ] 3D Objects <br>
-    [ ] User Input <br>
+    [X] User Input <br>
     [ ] cheat mode? <br>
     [ ] Lightning and smooth shading <br>
     [ ] Texture mapping <br>  
+    [X] Resizing <br> 
     extra features <br>
         [ ] Collision Detection AA BB <br>
         [ ] Head up Display <br>
--- SoccerGame.py
+++ SoccerGame.py
@@ -4,16 +4,16 @@ import time
 from OpenGL import GLUT as glut
 from OpenGL.GL import shaders, glScale
 from OpenGL.raw.GLUT import GLUT_KEY_LEFT, GLUT_KEY_RIGHT, GLUT_KEY_UP, GLUT_KEY_DOWN
-
 from Utilities2 import *
 
 
 class GameObject:
-	def __init__(self, position=glm.vec3(0.0), scale=glm.vec3(1.0, 1.0, 1.0), gravity=glm.vec3(0.0, 0.0, 0.0)):
-		self.position = position
-		self.scale = scale
+	def __init__(self, position=None, scale=None, gravity=None, color=None):
+		self.position = position or glm.vec3(0.0)
+		self.scale = scale or glm.vec3(1.0)
 		self.velocity = glm.vec3(0.0)
-		self.gravity = gravity
+		self.gravity = gravity or glm.vec3(0.0, 0.0, 0.0)
+		self.color = color or glm.vec3(0, 1, 0)
 
 	def get_transformation(self):
 		transformation = glm.mat4x4()
@@ -38,6 +38,7 @@ class GameObject:
 		gl.glDrawElements(gl.GL_TRIANGLES, 36, gl.GL_UNSIGNED_INT, None)
 
 	def draw(self):
+		gl.glUniform3fv(color_location, 1, glm.value_ptr(self.color))
 		# Set the transform uniform to self.transform
 		gl.glUniformMatrix4fv(transformation_location, 1, False, glm.value_ptr(self.get_transformation()))
 		# Draw the cube with an element array.
@@ -46,10 +47,10 @@ class GameObject:
 
 
 class Camera:
-	def __init__(self, position=glm.vec3(0.0), target=glm.vec3(0.0, 0.0, 1.0), up=glm.vec3(0.0, 1.0, 0.0)):
-		self.position = position
-		self.target = target
-		self.up = up
+	def __init__(self, position=None, target=None, up=None):
+		self.position = position or glm.vec3(0.0)
+		self.target = target or glm.vec3(0.0, 0.0, 1.0)
+		self.up = up or glm.vec3(0.0, 1.0, 0.0)
 
 	def get_view(self):
 		return glm.lookAt(self.position, self.target, self.up)
@@ -64,13 +65,14 @@ screen_size = glm.vec2(512, 512)
 glut.glutCreateWindow("Soccer Game")
 glut.glutReshapeWindow(int(screen_size.x), int(screen_size.y))
 
+# set initial variables for the score
 score = 0
-
 passed_time = 0.0
 
 
 # Set callback functions
 def display():
+	# compute the time which passed
 	global passed_time
 	delta_time = time.perf_counter() - passed_time
 	passed_time += delta_time
@@ -81,11 +83,14 @@ def display():
 	gl.glUniformMatrix4fv(projection_location, 1, False, glm.value_ptr(perspective_projection))
 
 	# Rotate the camera around the z-axis
-	camera_rotation = glm.rotate(glm.mat4x4(), glm.radians(time.perf_counter() * 42.0), glm.vec3(0.0, 1.0, 0.0))
+	# camera_rotation = glm.rotate(glm.mat4x4(), glm.radians(time.perf_counter() * 42.0), glm.vec3(0.0, 1.0, 0.0))
 
 	gl.glUniformMatrix4fv(view_location, 1, False, glm.value_ptr(camera.get_view()))
 
+	# sets the direction of the player
 	direction = glm.vec3(0.0)
+
+	# modifies the direction via keyboard input
 	if b'w' in pressed_keys:
 		direction += glm.vec3(0, 0, 2)
 	if b's' in pressed_keys:
@@ -112,46 +117,59 @@ def display():
 	player.translate(player.position)
 	player.velocity *= 0.96
 
-	# update the velocity / gravity
-	# ball.position.y = ball.gravity.y * delta_time
-	# ball.gravity *= 0.96
+	# rounding the values to avoid issues with too small numbers
+	ball.velocity.y = round(ball.velocity.y, 4)
+	ball.velocity.x = round(ball.velocity.x, 4)
+	ball.velocity.z = round(ball.velocity.z, 4)
+
+	# player
+	player.velocity.y = round(player.velocity.y, 4)
+	player.velocity.x = round(player.velocity.x, 4)
+	player.velocity.z = round(player.velocity.z, 4)
 
 	# check the collision with the ground or if the numbers gets too small
 	if check_collision(ball, ground) or ball.position.y < 0.01:
 		ball.position.y = 0.0
 		ball.velocity *= 0.96
+
+	# if ball is in the air gravity will decrease the height of the object
 	else:
 		ball.velocity += ball.gravity * delta_time
 
 	ball.position += ball.velocity * delta_time
 	ball.translate(ball.position)
 
+	if check_collision(obstacle, ball):
+		ball.position = glm.vec3(-5, 0, -10)
+		ball.velocity *= 0
+
+	if check_collision(obstacle, player):
+		player.velocity = direction * -(glm.normalize(player.position) * 40.0)
+
 	if check_collision(player, ball):
 		# changes the direction of the ball + adds velocity
 		ball.velocity = (glm.normalize(ball.position - player.position) * 7.0 - ball.gravity * 0.5)
 
-	# check collision with the sourunding
+	# check collision with the surrounding
 	if check_collision(player, wall_right) or check_collision(player, wall_left) or \
 			check_collision(player, wall_side_right) or check_collision(player, wall_side_left):
 		# change the direction and velocity after collision
 		player.velocity = direction * -(glm.normalize(player.position) * 40.0)
 
 	# check collision player and goal right
-	if check_collision(player, goal_right):
+	if check_collision(goal_right, player):
 		# change the direction and velocity after collision
 		player.velocity = direction * -(glm.normalize(goal_right.position - player.position) * 40.0)
 
 	# check collision player goal left
-	if check_collision(player, goal_left):
+	if check_collision(goal_left, player ):
 		player.velocity = direction * -(glm.normalize(goal_left.position - player.position) * 40.0)
 
 	# check collision with the goal
-	if check_collision(ball, goal_left) or check_collision(ball, wall_right)\
+	if check_collision(ball, goal_left) or check_collision(ball, wall_right) \
 			or check_collision(ball, wall_side_left) or check_collision(ball, wall_side_right):
 		# change the direction and velocity after collision
 		ball.velocity = direction * -(glm.normalize(ball.position) * 20.0)
-
-
 
 	if goal_left.position.x < ball.position.x < goal_right.position.x and ball.position.z > goal_right.position.z:
 		global score
@@ -159,20 +177,40 @@ def display():
 		ball.position = glm.vec3(-5, 0, -10)
 		ball.velocity *= 0.0
 
-	# print(player.position, ball.position)
-	glScale(3.0, 0.0, 0.0)
-	# order shouldn't matter
-	ground.draw()
-	goal_left.draw()
-	goal_right.draw()
-	ball.draw()
-	player.draw()
-	wall_left.draw()
-	wall_right.draw()
-	wall_side_right.draw()
-	wall_side_left.draw()
-	wall_backside.draw()
+	if ball.position.z < wall_backside.position.z or ball.position.z > wall_right.position.z or ball.position.z > wall_left.position.z:
+		ball.position = glm.vec3(-5, 0, -10)
+		ball.velocity *= 0
+	# glScale(3.0, 0.0, 0.0)
 
+	# draw the objects
+	ground.draw()
+	# 0.0 0.5 0.0
+	goal_left.draw()
+	# 0.0 1.0 0.0
+	goal_right.draw()
+	# 0.0 1.0 0.0
+
+	wall_left.draw()
+	# 1.0 1.0 0.0
+
+	wall_right.draw()
+	# 1.0 1.0 0.0
+	wall_side_right.draw()
+	# 0.5 0.0 0.0
+	wall_side_left.draw()
+	# 0.5 0.0 0.0
+	# color = (gl.GLfloat * 3)()
+	# gl.glGetUniformfv(shader_program, color_location, color)
+	# print(*color)
+	wall_backside.draw()
+	# 0.0 0.0 0.0
+	obstacle.draw()
+
+	player.draw()
+	# 0.5 0.5 0.0
+
+	ball.draw()
+	# 0.0 0.0 1.0
 
 
 	# Swap the buffer we just drew on with the one showing on the screen
@@ -183,6 +221,7 @@ glut.glutDisplayFunc(display)
 glut.glutIdleFunc(display)
 
 
+# adds the possibility to increase / decrease the windows of the game and it resize
 def resize(width, height):
 	gl.glViewport(0, 0, width, height)
 	screen_size.x = width
@@ -191,6 +230,7 @@ def resize(width, height):
 	perspective_projection = glm.perspective(glm.radians(45.0), screen_size.x / screen_size.y, 0.1, 100.0)
 
 
+# activate the function
 glut.glutReshapeFunc(resize)
 
 
@@ -198,16 +238,20 @@ def check_collision(object1, object2):  # AABB - AABB collision
 	# Collision x-axis?
 	collisionX = object1.position.x + (object1.scale.x * 2) >= object2.position.x and object2.position.x + (
 			object2.scale.x * 2) >= object1.position.x
+
 	#  Collision y-axis?
 	collisionY = object1.position.y + (object1.scale.y * 2) >= object2.position.y and object2.position.y + (
 			object2.scale.y * 2) >= object1.position.y
+
 	#  Collision z-axis?
 	collisionZ = object1.position.z + (object1.scale.z * 2) >= object2.position.z and object2.position.z + (
 			object2.scale.z * 2) >= object1.position.z
-	# Collision only if on both axes
+
+	# Collision only if on all three axes
 	return collisionX and collisionY and collisionZ
 
 
+# creates a set for the keys to be pressed at the same time
 pressed_keys = set()
 
 
@@ -216,21 +260,18 @@ def keyboard_up_input(key, x, y):
 	pressed_keys.remove(key)
 
 
+# enables the user to change the position of the camera via keyboard input
 def handleSpecialKeypress(key, x, y):
 	if key == GLUT_KEY_LEFT and not camera.position.x == 24:
-		print(camera.position)
 		camera.position += glm.vec3(10.0, 0.0, 0.0)
 
 	if key == GLUT_KEY_RIGHT and not camera.position.x == -26:
-		print(camera.position)
 		camera.position -= glm.vec3(10.0, 0.0, 0.0)
 
 	if key == GLUT_KEY_UP and not camera.position.y == 60:
-		print(camera.position)
 		camera.position += glm.vec3(0.0, 10.0, 0.0)
 
 	if key == GLUT_KEY_DOWN and not camera.position.y == 0:
-		print(camera.position)
 		camera.position -= glm.vec3(0.0, 10.0, 0.0)
 
 
@@ -251,6 +292,7 @@ def keyboard_input(key, x, y):
 
 	if key == b'r':
 		ball.position = glm.vec3(-5, 0, -10)
+		ball.velocity *= 0
 
 	if key == b'\x1b':
 		print("The score is ", score)
@@ -263,40 +305,29 @@ glut.glutKeyboardFunc(keyboard_input)
 # Compile shaders [shorthand]
 vertex_shader = shaders.compileShader("""
 attribute vec3 position;
-attribute vec4 color;
+
 uniform mat4 projection;
 uniform mat4 view;
 uniform mat4 transformation;
+uniform vec3 color;
 
-varying vec4 vertex_color;
-varying vec3 vertex_position;
+varying vec3 frag_color;
 
 void main()
 {
-    //  Because the transformation matrix is 4x4, we have to construct a vec4 from position, so we an multiply them
-    vec4 pos = vec4(position, 1.0);
-    gl_Position = projection * view * transformation * pos;
-
-    vertex_color = color;
-    vertex_position = position;
+	//  Because the transformation matrix is 4x4, we have to construct a vec4 from position, so we an multiply them
+	vec4 pos = vec4(position, 1.0);
+	gl_Position = projection * view * transformation * pos;
+	frag_color = color;
 }
 """, gl.GL_VERTEX_SHADER)
 
 fragment_shader = shaders.compileShader("""
-varying vec4 vertex_color;
-varying vec3 vertex_position;
+varying vec3 frag_color;
 
 void main()
 {
-    vec4 color = vertex_color;
-    vec3 p = vertex_position;
-    float m = 0.5;
-
-    if((p.x < -m || p.x > m) && (p.y < -m || p.y > m) && (p.z < -m || p.z > m)) {
-        color = ceil(color);
-    }
-
-    gl_FragColor = color;
+	gl_FragColor = vec4(frag_color, 1.0);
 }
 """, gl.GL_FRAGMENT_SHADER)
 
@@ -324,12 +355,13 @@ data_offset = gl.ctypes.c_void_p(0)  # 0, beginning offset
 # Request a buffer slot from GPU
 vertex_buffer = gl.glGenBuffers(1)
 
+# gets the position location
 position_location = gl.glGetAttribLocation(shader_program, "position")
 
 # Describe how the position attribute will parse this buffer
 # 1. First, tell where the data will be read from
 gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vertex_buffer)
-# 2. Second, tell how the data should be read and where to be sent  s for this case, therefore 0)
+# 2. Second, tell how the data should be read and where to be sent for this case, therefore 0)
 gl.glVertexAttribPointer(position_location, data_count, gl.GL_FLOAT, False, data_stride, data_offset)
 
 # Enable the attribute from position_location
@@ -337,36 +369,6 @@ gl.glEnableVertexAttribArray(position_location)
 
 # Send vertex data (which is on CPU memory) to vertex buffer (which is on GPU memory)
 gl.glBufferData(gl.GL_ARRAY_BUFFER, vertex_data.nbytes, vertex_data, gl.GL_STATIC_DRAW)
-
-# Create the color  data. It is 8 RGBA values (for each corner of the cube)
-color_data = np.array(
-	[
-		(1, 1, 1, 1), (0, 1, 1, 1), (0, 0, 1, 1), (1, 0, 1, 1),
-		(1, 0, 0, 1), (1, 1, 0, 1), (0, 1, 0, 1), (0, 0, 0, 1),
-	],
-	dtype=np.float32
-)
-# Get properties of the data container
-data_count = color_data.shape[1]  # 4, the count of floats per vertex
-data_stride = color_data.strides[0]  # 16, bytes to skip for the next color data
-data_offset = gl.ctypes.c_void_p(0)  # 0, beginning offset
-
-# Request a buffer slot from GPU
-color_buffer = gl.glGenBuffers(1)
-
-color_location = gl.glGetAttribLocation(shader_program, "color")
-
-# Describe how the position attribute will parse this buffer
-# 1. First, tell where the data will be read from
-gl.glBindBuffer(gl.GL_ARRAY_BUFFER, color_buffer)
-# 2. Second, tell how the data should be read and where to be sent color_location for this case, therefore 1)
-gl.glVertexAttribPointer(color_location, data_count, gl.GL_FLOAT, False, data_stride, data_offset)
-
-# Enable the attribute at color_location
-gl.glEnableVertexAttribArray(color_location)
-
-# Send color data (which is on CPU memory) to color buffer (which is on GPU memory)
-gl.glBufferData(gl.GL_ARRAY_BUFFER, color_data.nbytes, color_data, gl.GL_STATIC_DRAW)
 
 # Create the element data. It is an indexing of the vertex data. The indexes will be grouped by 3 to create triangles.
 element_data = np.array(
@@ -396,11 +398,15 @@ view_location = gl.glGetUniformLocation(shader_program, "view")
 # Get the projection location
 projection_location = gl.glGetUniformLocation(shader_program, "projection")
 
+# Get the color location
+color_location = gl.glGetUniformLocation(shader_program, "color")
+
 # Configure GL -----------------------------------------------------------------------|
 
 # Enable depth test
 gl.glEnable(gl.GL_DEPTH_TEST)
-# Accept fragment if it closer to the camera than the former one
+
+# Accept fragment if it is closer to the camera than the former one
 gl.glDepthFunc(gl.GL_LESS)
 
 # Create Camera and Game Objects -----------------------------------------------------|
@@ -412,14 +418,16 @@ camera = Camera(position=glm.vec3(4.0, 30.0, -40.0), target=glm.vec3(0.0))
 player = (
 	GameObject(
 		position=glm.vec3(0, 0, -10),
-		scale=glm.vec3(1)
+		scale=glm.vec3(1),
+		color=glm.vec3(0.5, 0.5, 0.0)
 	)
 )
 
 ground = (
 	GameObject(
 		position=glm.vec3(0, -101, 0),
-		scale=glm.vec3(100)
+		scale=glm.vec3(100),
+		color=glm.vec3(0.0, 0.5, 0.0)
 	)
 )
 
@@ -427,59 +435,72 @@ ball = (
 	GameObject(
 		position=glm.vec3(-5, 0, -10),
 		scale=glm.vec3(1),
-		gravity=glm.vec3(0.0, -10.0, 0.0)
+		gravity=glm.vec3(0.0, -10.0, 0.0),
+		color=glm.vec3(0.0, 0.0, 1.0)
 	)
 )
 
 goal_left = (
 	GameObject(
 		position=glm.vec3(-5, 0, 30),
-		scale=glm.vec3(1)
+		scale=glm.vec3(1),
+		color=glm.vec3(0.0, 1.0, 0.0)
 	)
 )
 goal_right = (
 	GameObject(
 		position=glm.vec3(5, 0, 30),
-		scale=glm.vec3(1)
+		scale=glm.vec3(1),
+		color=glm.vec3(0.0, 1.0, 0.0)
 	)
 )
 
 wall_left = (
 	GameObject(
 		position=glm.vec3(15, 0, 30),
-		scale=glm.vec3(9.0, 1.0, 1.0)
+		scale=glm.vec3(9.0, 1.0, 1.0),
+		color=glm.vec3(1.0, 1.0, 0.0)
 	)
 )
-
 
 wall_right = (
 	GameObject(
 		position=glm.vec3(-15, 0, 30),
-		scale=glm.vec3(-9.0, 1.0, 1.0)
+		scale=glm.vec3(-9.0, 1.0, 1.0),
+		color=glm.vec3(1.0, 1.0, 0.0)
 	)
 )
 wall_side_right = (
 	GameObject(
 		position=glm.vec3(-25, 0, 6),
-		scale=glm.vec3(1.0, 1.0, 25.0)
+		scale=glm.vec3(1.0, 1.0, 25.0),
+		color=glm.vec3(0.5, 0.0, 0.0)
 	)
 )
 
 wall_side_left = (
 	GameObject(
 		position=glm.vec3(25, 0, 6),
-		scale=glm.vec3(1.0, 1.0, 25.0)
+		scale=glm.vec3(1.0, 1.0, 25.0),
+		color=glm.vec3(0.5, 0.0, 0.0)
 	)
 )
 
 wall_backside = (
 	GameObject(
 		position=glm.vec3(0, 0, -18),
-		scale=glm.vec3(24.0, 1.0, 1.0)
+		scale=glm.vec3(24.0, 1.0, 1.0),
+		color=glm.vec3(0.0, 0.0, 0.0)
 	)
 )
 
-
+obstacle = (
+	GameObject(
+		position=glm.vec3(0, 0, 0),
+		scale=glm.vec3(1.5, 1.5, 1.5),
+		color=glm.vec3(0.0, 0.0, 0.0)
+	)
+)
 
 # Start the main loop
 glut.glutMainLoop()
